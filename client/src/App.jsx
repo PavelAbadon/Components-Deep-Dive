cd03300313e9a4c31547cdac8889c7059dc93807
Pavel Vasilev <pavelabadon@gmail.com>
2025-11-07 16:02:23 +0200

    Add footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Footer from './components/Footer'
+
+
 
 function App() {
    return (
       <main>
         <Header/>
+
+        <Footer/>
+        
       </main>
     
   )
