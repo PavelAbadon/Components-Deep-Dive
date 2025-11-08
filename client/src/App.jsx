ac1758a1ac2020ae57a50913a856dca742d7e6f7
Pavel Vasilev <pavelabadon@gmail.com>
2025-11-08 10:17:47 +0200

    add structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Footer from './components/Footer'
+import Search from './components/Search'
+import TableComponent from './components/TableComponent'
+import Pagination from './components/Pagination'
 
 function App() {
    return (
-      <main>
-        <Header/>
+    <div>
+      <Header/>
+      <main className="main">
+        <section className="card users-container">        
+          <Search/>    
+          <TableComponent/>               
+          <Pagination/>
+        </section>        
       </main>
-    
+      <Footer/>
+    </div>
   )
 }
 
